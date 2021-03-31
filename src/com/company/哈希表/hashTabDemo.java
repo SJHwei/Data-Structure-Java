2ecbe8bad8f061aca3e307d77a6845a0f6e61304
SJHwei <2863254966@qq.com>
2021-03-31 15:35:05 +0800

    哈希表的创建和遍历

---
+++ src/com/company/哈希表/hashTabDemo.java
@@ -0,0 +1,115 @@
+package com.company.哈希表;
+
+import java.util.HashMap;
+
+/**
+ * @author ShiWei
+ * @date 2021/3/31 - 13:48
+ */
+public class hashTabDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * 创建HashTab管理多条链表
+ */
+class HashTab {
+    private EmplinkedList[] empLinkedListArray;
+    private int size; //表示有多少条链表
+
+    //构造器
+    public HashTab(int size) {
+        //初始化empLinkedListArray
+        empLinkedListArray = new EmplinkedList[size];
+        //？留一个坑
+    }
+
+    //添加雇员
+    public void add(Emp emp) {
+        //根据员工的id，得到该员工应当添加到哪条链表
+        int empLinkedListNo = hashFun(emp.id);
+        //将emp添加到对应的链表中
+        empLinkedListArray[empLinkedListNo].add(emp);
+    }
+
+    //遍历所有的链表，遍历hashtab
+    public void list() {
+        for (int i = 0; i < size; i++) {
+            empLinkedListArray[i].list();
+        }
+    }
+
+    //编写散列函数，使用一个简单取模法
+    public int hashFun(int id) {
+        return id % size;
+    }
+}
+
+//表示一个雇员
+class Emp {
+    public int id;
+    public String name;
+    public Emp next; //next默认为空
+
+    public Emp(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+//创建EmpLinkedList，表示链表
+class EmplinkedList {
+    //头指针，执行第一个Emp，因此我们这个链表的head是直接指向第一个Emp
+    private Emp head; //默认null
+
+    //添加雇员到链表
+    //说明：
+    //1.假定当添加雇员时，id是自增长，即id的分配总是从小到大
+    //因此我们将该雇员直接加入到本链接的最后即可
+    public void add(Emp emp) {
+        //如果是添加第一个雇员
+        if (head == null) {
+            head = emp;
+            return;
+        }
+        //如果不是第一个雇员，则使用一个辅助的指针，帮助定位到最后
+        Emp curEmp = head;
+        while (true) {
+            //说明到链表最后
+            if (curEmp.next == null) {
+                break;
+            }
+            //后移
+            curEmp = curEmp.next;
+        }
+        //退出时直接将emp加入链表
+        curEmp.next = emp;
+    }
+
+    /**
+     * 遍历链表的雇员信息
+     */
+    public void list() {
+        //说明链表为空
+        if (head == null) {
+            System.out.println("当前链表为空");
+            return;
+        }
+        System.out.println("当前链表的信息为");
+        //辅助指针
+        Emp curEmp = head;
+        while (true) {
+            System.out.printf("=> id=%d name=%s\t", curEmp.id, curEmp.name);
+            //说明curEmp已经是最后节点了
+            if (curEmp.next == null) {
+                break;
+            }
+            //后移，遍历
+            curEmp = curEmp.next;
+        }
+        System.out.println();
+    }
+}
