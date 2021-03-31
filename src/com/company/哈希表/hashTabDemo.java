7b8ad3c02a263e3d4fda7d68da397df23b6201ca
SJHwei <2863254966@qq.com>
2021-03-31 16:36:12 +0800

    哈希表的查找

--- src/com/company/哈希表/hashTabDemo.java
+++ src/com/company/哈希表/hashTabDemo.java
@@ -1,6 +1,7 @@
 package com.company.哈希表;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * @author ShiWei
@@ -10,6 +11,44 @@ public class hashTabDemo {
 
     public static void main(String[] args) {
 
+        //创建哈希表
+        HashTab hashTab = new HashTab(7);
+
+        //写一个简单的菜单
+        String key = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("add: 添加雇员");
+            System.out.println("list: 显示雇员");
+            System.out.println("find: 查找雇员");
+            System.out.println("exit: 退出系统");
+
+            key = scanner.next();
+            switch (key) {
+                case "add":
+                    System.out.print("输入id: ");
+                    int id = scanner.nextInt();
+                    System.out.print("输入名字: ");
+                    String name = scanner.next();
+                    //创建雇员
+                    Emp emp = new Emp(id, name);
+                    hashTab.add(emp);
+                    break;
+                case "list":
+                    hashTab.list();
+                    break;
+                case "find":
+                    System.out.print("请输入要查找的id: ");
+                    id = scanner.nextInt();
+                    hashTab.findEmpById(id);
+                    break;
+                case "exit":
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    break;
+            }
+        }
     }
 }
 
@@ -24,7 +63,11 @@ class HashTab {
     public HashTab(int size) {
         //初始化empLinkedListArray
         empLinkedListArray = new EmplinkedList[size];
-        //？留一个坑
+        this.size = size;
+        //？留一个坑，这时不要忘了分别初始化每一个链表
+        for (int i = 0; i < size; i++) {
+            empLinkedListArray[i] = new EmplinkedList();
+        }
     }
 
     //添加雇员
@@ -38,7 +81,19 @@ class HashTab {
     //遍历所有的链表，遍历hashtab
     public void list() {
         for (int i = 0; i < size; i++) {
-            empLinkedListArray[i].list();
+            empLinkedListArray[i].list(i);
+        }
+    }
+
+    //根据输入的id，查找雇员
+    public void findEmpById(int id) {
+        //使用散列函数确定到哪条链表查找
+        int empLinkedListNo = hashFun(id);
+        Emp emp = empLinkedListArray[empLinkedListNo].findEmpById(id);
+        if (emp != null) { //找到
+            System.out.printf("在第%d条链表中找到雇员 id = %d\n", empLinkedListNo + 1, id);
+        } else {
+            System.out.println("在哈希表中，没有找到该雇员~");
         }
     }
 
@@ -92,13 +147,13 @@ class EmplinkedList {
     /**
      * 遍历链表的雇员信息
      */
-    public void list() {
+    public void list(int no) {
         //说明链表为空
         if (head == null) {
-            System.out.println("当前链表为空");
+            System.out.println("第" + (no + 1) + "链表为空");
             return;
         }
-        System.out.println("当前链表的信息为");
+        System.out.print("第" + (no + 1) + "链表的信息为");
         //辅助指针
         Emp curEmp = head;
         while (true) {
@@ -111,5 +166,33 @@ class EmplinkedList {
             curEmp = curEmp.next;
         }
         System.out.println();
+    }
+
+    //根据id查找雇员
+    //如果查找到，就返回Emp，如果没有找到，就返回null
+    public Emp findEmpById(int id) {
+        //判断链表是否为空
+        if (head == null) {
+            System.out.println("链表为空");
+            return null;
+        }
+
+        //辅助指针
+        Emp curEmp = head;
+        while (true) {
+            if (curEmp.id == id) { //找到
+                break; //这时curEmp就指向要查找的雇员
+            }
+
+            //退出
+            if (curEmp.next == null) { //说明遍历当前链表没有找到该雇员
+                curEmp = null;
+                break;
+            }
+
+            curEmp = curEmp.next; //后移
+        }
+
+        return curEmp;
     }
 }
